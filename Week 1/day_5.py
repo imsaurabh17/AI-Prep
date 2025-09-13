37326370412f85c106f25b5005ee48323f732d9f
Saurabh Dubey <saurabhdubey222786@gmail.com>
2025-09-13 10:28:52 +0530

    day 5 prep

---
+++ Week 1/day_5.py
@@ -0,0 +1,19 @@
+class BankAccount:
+
+    def __init__(self,accountnumber:int,balance:int):
+        self.an = accountnumber
+        self.balance = balance
+
+    def deposit(self,amount:int)->int:
+        self.balance += amount
+        return self.balance
+    
+    def withdraw(self,amount:int)->int:
+        self.balance -= amount
+        return self.balance
+    
+    def get_balance(self)->int:
+        return self.balance
+    
+Bank = BankAccount(1234,1000)
+print(Bank.withdraw(800))
